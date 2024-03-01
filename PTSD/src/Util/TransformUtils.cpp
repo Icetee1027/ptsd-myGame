e2d5585433a44eda0ad152f0725ea39b91d5d055
Icetee1027 <921027leo@gmail.com>
2024-03-01 15:40:30 +0800

    v0.0.0.20240301

--- PTSD/src/Util/TransformUtils.cpp
+++ PTSD/src/Util/TransformUtils.cpp
@@ -7,14 +7,26 @@ Core::Matrices ConvertToUniformBufferData(const Util::Transform &transform,
                                           const glm::vec2 &size,
                                           const float zIndex, glm::vec3 m_View,
                                           Core::Drawable::World state) {
-  /*  constexpr glm::mat4 eye(1.F);
-    constexpr float nearClip = -100;
-    constexpr float farClip = 100;
-    auto view = glm::translate(eye, m_View);
-    auto projection = glm::perspective(glm::radians(90.0f),
-                                       static_cast<float>(WINDOW_WIDTH) /
-                                           static_cast<float>(WINDOW_HEIGHT),
-                                       -200.0f - m_View.z, 200.0f - m_View.z);
+    constexpr glm::mat4 eye(1.F);
+    struct glm::mat<4, 4, glm::f32, glm::packed_highp> view;
+    struct glm::mat<4, 4, glm::f32, glm::packed_highp> projection;
+
+    if (state == Core::Drawable::World::OBJECT) {
+        view = glm::translate(eye, m_View);
+        projection =
+            glm::perspective(glm::radians(90.0f),
+                                static_cast<float>(WINDOW_WIDTH) /
+                                    static_cast<float>(WINDOW_HEIGHT),
+                                -200.0f - m_View.z, 100.0f - m_View.z);
+        
+    } else {
+        projection = glm::ortho<float>(0.0F, 1.0F, 0.0F, 1.0F, -100.0f, 100.0f);
+        view =
+            glm::scale(eye,
+                        {1.F / WINDOW_WIDTH, 1.F / WINDOW_HEIGHT, 1.F}) *
+            glm::translate(eye, {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0});
+        
+    }
     auto model =
         glm::translate(eye, {transform.translation.x, transform.translation.y,
                              transform.translation.z + zIndex / 50}) *
@@ -25,49 +37,7 @@ Core::Matrices ConvertToUniformBufferData(const Util::Transform &transform,
         model,
         projection * view,
     };
-    return data;*/
-    constexpr glm::mat4 eye(1.F);
-    constexpr float nearClip = -100;
-    constexpr float farClip = 100;
-    if (state == Core::Drawable::World::OBJECT) {
-        auto view = glm::translate(eye, m_View);
-        auto projection =
-            glm::perspective(glm::radians(90.0f),
-                                static_cast<float>(WINDOW_WIDTH) /
-                                    static_cast<float>(WINDOW_HEIGHT),
-                                -200.0f - m_View.z, 200.0f - m_View.z);
-        auto model = glm::translate(
-                            eye, {transform.translation.x, transform.translation.y,
-                                transform.translation.z + zIndex / 50}) *
-                        glm::rotate(eye, transform.rotation, glm::vec3(0, 0, 1)) *
-                        glm::scale(eye, {transform.scale * size, 1});
-
-        Core::Matrices data = {
-            model,
-            projection * view,
-        };
-        return data;
-    } else {
-        auto projection =
-            glm::ortho<float>(0.0F, 1.0F, 0.0F, 1.0F, nearClip, farClip);
-        auto view =
-            glm::scale(eye,
-                        {1.F / WINDOW_WIDTH, 1.F / WINDOW_HEIGHT, 1.F}) *
-            glm::translate(eye, {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0});
-        auto model =
-            glm::translate(eye, {transform.translation.x,
-                                    transform.translation.y,
-                                    transform.translation.z + zIndex / 50}) *
-            glm::rotate(eye, transform.rotation, glm::vec3(0, 0, 1)) *
-            glm::scale(eye, {transform.scale * size, 1});
-
-        Core::Matrices data = {
-            model,
-            projection * view,
-        };
-        return data;
-    }
-
+    return data;
 
     // TODO: TRS comment
     
