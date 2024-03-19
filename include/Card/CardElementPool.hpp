892dbf5a7e655c2afbb8e5d6f447f8b2e4ab444a
Icetee1027 <921027leo@gmail.com>
2024-03-19 09:25:52 +0800

    Update CardNumber  color

--- include/Card/CardElementPool.hpp
+++ include/Card/CardElementPool.hpp
@@ -2,24 +2,36 @@
 #define CARDELEMENTPOOL_HPP
 
 #include <unordered_map>
+#include<map>
 #include <memory>
+#include <utility>
 #include "Util/Image.hpp"
 #include "Util/Text.hpp"
 #include "Card/Card.hpp"
 #include "Util/color.hpp"
-
+namespace std {
+    template <>
+    struct hash<std::pair<int, int>> {
+        size_t operator()(const std::pair<int, int>& p) const {
+            size_t h1 = std::hash<int>{}(p.first);
+            size_t h2 = std::hash<int>{}(p.second);
+            return h1 ^ (h2 << 1);
+        }
+    };
+}
 namespace card {
     class CardElementPool {
     private:
         std::unordered_map<std::string, std::pair<std::weak_ptr<Util::Image>, std::weak_ptr<Util::Text>>> elements;
-        std::unordered_map<int, std::weak_ptr<Util::Text>> numberTextElements;
+        std::unordered_map<std::pair<int, int>, std::weak_ptr<Util::Text>, std::hash<std::pair<int, int>>> numberTextElements;
         static const std::unordered_map<Type, glm::vec4> NumberColor;
+        static const std::map<int, Util::Colors> m_Colors;
 
     public:
-        std::pair<std::shared_ptr<Util::Image>, std::shared_ptr<Util::Text>> getElement(const std::string& name);
+        std::pair<std::shared_ptr<Util::Image>, std::shared_ptr<Util::Text>> getElement(const std::string& name,int color);
 
 
-        std::shared_ptr<Util::Text> getNumberTextElement(int number);
+        std::shared_ptr<Util::Text> getNumberTextElement(int number, int color);
     };
 }
 
